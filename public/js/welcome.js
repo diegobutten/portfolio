338b6ce31571775c28c1effe1e3e80d930c47ec6
Darrel Naz <diegobutten23@gmail.com>
2021-06-12 00:03:20 +0800

    thank you lord

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -64,3 +64,20 @@ $(document).on({
 		$(this).removeClass('z-10')
 	},
 }, '.language')
+
+
+
+fetch('/users', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		name: 'User 1'
+	})
+})
+.then(res => {
+	return res.json()
+})
+.then(data => console.log(data))
+.catch(error => console.log('ERROR')) 
