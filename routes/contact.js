befc963faacb367dabefe4f4e202f57bfb7a220b
Darrel <diegobutten23@gmail.com>
2026-07-11 09:34:54 +0800

    gmail backend editing

--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,21 @@
 const router = require("express").Router()
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
